3197959bd72576e1176b620328ec6be7084f0bb7
Hamza El Ghdayr <hael-ghd@e3r3p17.1337.ma>
2024-06-25 01:41:41 +0100

    Init

---
+++ philo/philo.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hael-ghd <hael-ghd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 20:50:32 by hael-ghd          #+#    #+#             */
+/*   Updated: 2024/06/24 23:21:06 by hael-ghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	help_func(int len_str, char **str)
+{
+	int	i;
+	int	s;
+	int	l;
+
+	s = 1;
+	while (s < len_str)
+	{
+		l = 1;
+		i = 0;
+		while (str[s][i] != 0)
+		{
+			if (str[s][i] >= 48 && str[s][i] <= 57)
+				l = 0;
+			if (str[s][i] != 32 && str[s][i] != 43)
+				if (str[s][i] < 48 || str[s][i] > 57)
+					return (-1);
+			i++;
+		}
+		if (l == 1)
+			return (-1);
+		s++;
+	}
+	return (0);
+}
+
+int	check_args(int argc, char **argv)
+{
+	int	i;
+	int	s;
+
+	i = 1;
+	if (help_func(argc, argv) == -1)
+		return (-1);
+	while (i < argc)
+	{
+		s = 0;
+		while (argv[i][s] != 0)
+		{
+			if (argv[i][s] == 43 && s - 1 >= 0)
+				if (argv[i][s - 1] != 32)
+					return (-1);
+			if (argv[i][s] == 43)
+				if (argv[i][++s] == 32 || argv[i][s] == 43 || argv[i][s] == 0)
+					return (-1);
+			s++;
+		}
+		i++;
+	}
+	if (i == argc)
+		return (0);
+	return (-1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 5 && ac != 6)
+		printf("Error: invalid arguments number\n");
+	if (check_args(ac, av) == -1)
+		printf("Error: invalid arguments\n");
+}
---
+++ philo/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hael-ghd <hael-ghd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/24 20:40:04 by hael-ghd          #+#    #+#             */
+/*   Updated: 2024/06/24 23:19:15 by hael-ghd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <string.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
