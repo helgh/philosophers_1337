a4924bf67c02ce4b9355ac1f2c952fa86cdb6aa0
Hamza El Ghdayr <hael-ghd@e3r3p9.1337.ma>
2024-07-27 05:45:54 +0100

    Finish✅

--- philo/monitor_function.c
+++ philo/monitor_function.c
@@ -6,7 +6,7 @@
 /*   By: hael-ghd <hael-ghd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 06:49:57 by hael-ghd          #+#    #+#             */
-/*   Updated: 2024/07/27 03:05:34 by hael-ghd         ###   ########.fr       */
+/*   Updated: 2024/07/27 05:45:29 by hael-ghd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,6 @@ int	check_philo(t_philo *philo)
 		pthread_mutex_lock(&philo->info->dead);
 		died_philo(philo);
 		philo->info->died_philo = -1;
-		pthread_mutex_unlock(&philo->info->dead);
 		return (-1);
 	}
 	return (0);
@@ -70,7 +69,6 @@ int	check_nbr_eat(t_philo *philo)
 	{
 		pthread_mutex_lock(&philo->info->dead);
 		philo->info->died_philo = -1;
-		pthread_mutex_unlock(&philo->info->dead);
 		return (-1);
 	}
 	return (0);
